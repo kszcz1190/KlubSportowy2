63a5ca3b7ba2a09138b00cc5efa5d7c42e3d06df
oreols <oreak1500@gmail.com>
2024-10-20 23:30:08 +0200

    modele zawodnik i staty

---
+++ Models/StatystykiZawodnikaMeczModel.cs
@@ -0,0 +1,19 @@
+﻿namespace KlubSportowy.Models
+{
+    public class StatystykiZawodnikaMeczModel
+    {
+        public int Id { get; set; }
+        public int MeczModelId { get; set; }
+        public MeczModel MeczModel { get; set; }
+        public int ZawodnikModelId { get; set; }
+        public ZawodnikModel ZawodnikModel { get; set; }
+        public int IloscGoli { get; set; }
+        public int IloscZoltychKartek { get; set; }
+        public int IloscCzerwonychKartek { get; set; }
+        public int IloscMinutRozegranych { get; set; }
+        public bool CzyZawodnikZagralWMeczu { get; set; }
+        public bool CzyKapitan { get; set; }
+        public string Pozycja { get; set; }
+
+    }
+}
---
+++ Models/ZawodnikModel.cs
@@ -0,0 +1,21 @@
+﻿namespace KlubSportowy.Models
+{
+    public class ZawodnikModel
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int Wiek { get; set; }
+        public string Kraj { get; set; }
+
+        public string Pozycja { get; set; }
+        public int LacznaIloscGoli { get; set; }
+        public int LacznaIloscZoltychKartek { get; set; }
+        public int LacznaIloscCzerwonychKartek { get; set; }
+        public int LacznaIloscMeczyRozegranych { get; set; }
+        public int LacznaIloscMinutRozegranych { get; set; }
+        public int NumerZawodnika { get; set; }
+
+
+    }
+}
