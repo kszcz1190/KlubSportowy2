c10add886941108ecbff495a9d51e4f4617097c4
Szczepaniak_Karolina <karolina.szczepaniak468@gmail.com>
2024-11-19 22:17:24 +0100

    przekierowanie po rejestracji

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -189,21 +189,7 @@ namespace KlubSportowy.Areas.Identity.Pages.Account
                         await _roleManager.CreateAsync(new IdentityRole(selectedRole));
                     }
 
-                    // Przypisz rolę do użytkownika
                     await _userManager.AddToRoleAsync(user, selectedRole);
-
-                    // Potwierdzenie e-maila i dalsze akcje
-                    //var userId = await _userManager.GetUserIdAsync(user);
-                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    //var callbackUrl = Url.Page(
-                    //    "/Account/ConfirmEmail",
-                    //    pageHandler: null,
-                    //    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                    //    protocol: Request.Scheme);
-
-                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
@@ -211,8 +197,8 @@ namespace KlubSportowy.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
+                        return RedirectToAction("Index", "ZawodnikModels");
+                       
                     }
                 }
 
